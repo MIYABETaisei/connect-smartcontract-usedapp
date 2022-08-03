3b83244eec7a99cf3c9bb167745b3f9bcfea1fb5
MIYABETaisei <mytspe46@gmail.com>
2022-08-03 11:11:34 +0900

    ✨ connect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { DAppProvider } from "@usedapp/core";
+
+const config = {
+  multicallAddresses: ["0x5FbDB2315678afecb367f032d93F642f64180aa3"],
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DAppProvider config={config}>
+      <Component {...pageProps} />
+    </DAppProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
